8c6c017dbc88a5e09f1254b57765e91f11647a06
DanieleAntonaci <danieleanto96@gmail.com>
2022-11-16 16:03:58 +0100

    Bonus scelta difficoltá

---
+++ Bonus/main.js
@@ -0,0 +1,69 @@
+'use strict'
+const container = document.querySelector('.container');
+const btnPlay = document.querySelector('button#play');
+
+function createBox() {
+    const divBox = document.createElement('div');
+
+    return divBox
+}
+
+// bottone per far comparire la griglia
+btnPlay.addEventListener('click', function () {
+    container.classList.remove('hidden');
+    let difficultyLevel = document.querySelector('#difficulty-level').value;
+    if (difficultyLevel === 'easy') {
+        for (let i = 0; i < 100; i++) {
+            // crea l'elemento div
+            let boxNumerato = createBox();
+            // lo numera
+            boxNumerato.innerHTML = `${i + 1}`;
+            // aggiunge la classe box
+            boxNumerato.classList.add('box');
+            boxNumerato.classList.add('easy');
+
+            // bottone che cambia il colore
+            boxNumerato.addEventListener('click', function () {
+                this.classList.toggle('clicked')
+                console.log(`Hai selzionato la casella ${i + 1}`);
+            })
+            container.appendChild(boxNumerato)
+        };
+    } else if (difficultyLevel === 'medium') {
+        for (let i = 0; i < 90; i++) {
+            // crea l'elemento div
+            let boxNumerato = createBox();
+            // lo numera
+            boxNumerato.innerHTML = `${i + 1}`;
+            // aggiunge la classe box
+            boxNumerato.classList.add('box');
+            boxNumerato.classList.add('medium');
+
+            // bottone che cambia il colore
+            boxNumerato.addEventListener('click', function () {
+                this.classList.toggle('clicked')
+                console.log(`Hai selzionato la casella ${i + 1}`);
+            })
+            container.appendChild(boxNumerato)
+        };
+    } else if (difficultyLevel === 'hard') {
+        for (let i = 0; i < 70; i++) {
+            // crea l'elemento div
+            let boxNumerato = createBox();
+            // lo numera
+            boxNumerato.innerHTML = `${i + 1}`;
+            // aggiunge la classe box
+            boxNumerato.classList.add('box');
+            boxNumerato.classList.add('hard');
+
+            // bottone che cambia il colore
+            boxNumerato.addEventListener('click', function () {
+                this.classList.toggle('clicked')
+                console.log(`Hai selzionato la casella ${i + 1}`);
+            })
+            container.appendChild(boxNumerato)
+        };
+    };
+
+
+});
